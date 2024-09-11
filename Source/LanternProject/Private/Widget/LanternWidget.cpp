3f5786877057aeb08113e7ac344cfb418bcd5abe
fl4meboi <djkim9876@gmail.com>
2024-09-11 03:19:17 -0400

    KDJ: Works up to 16 char + image but cannot handle only text

--- Source/LanternProject/Private/Widget/LanternWidget.cpp
+++ Source/LanternProject/Private/Widget/LanternWidget.cpp
@@ -22,33 +22,87 @@ void ULanternWidget::InitWidget(UTexture2D* Texture, const FString& Text)
 	{
 		FString NewText = Text.Replace(TEXT("\\n"), TEXT("\n"));
 
-		if (NewText.Len() > 17)
+		if (Texture != nullptr)			// Case 1: uploaded image, up to 16 char in 2 text boxes
 		{
-			// Text + Text : Index 1
-
-			FString FirstPart = NewText.Left(8);
-			FString RemainingPart = NewText.Mid(8);
-
-			WidgetSwitcher->SetActiveWidgetIndex(1);
-			TB_Text_1->SetText(FText::FromString(FirstPart));
-			TB_Text_2->SetText(FText::FromString(RemainingPart));
-
-			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Over_8_Char"));
-		}
-		else
-		{
-			// Image + Text : Index 0
-
-			FString FirstPart = NewText.Left(8);
-			FString RemainingPart = NewText.Mid(8);
-
 			WidgetSwitcher->SetActiveWidgetIndex(0);
 			Img_Image->SetBrushFromTexture(Texture);
-			TB_Text_1->SetText(FText::FromString(FirstPart));
-			TB_Text_2->SetText(FText::FromString(RemainingPart));
 
-			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Under_8_Char"));
+			TB_Text_1->SetText(FText::FromString(NewText.Left(8)));		// First 8 char
+
+			if (NewText.Len() > 8)
+			{
+				TB_Text_2->SetText(FText::FromString(NewText.Mid(8, 8)));	// Next 8 char
+			}
+			else
+			{
+				TB_Text_2->SetText(FText::GetEmpty());
+			}
+
+			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - Image with up to 16 characters of text"));
 		}
+		else		// Case 2: no image, up to 32 char in 4 text boxes
+		{
+			WidgetSwitcher->SetActiveWidgetIndex(1);
+
+			TB_Text_3->SetText(FText::FromString(NewText.Left(8)));		// First 8 char
+
+			if (NewText.Len() > 8)
+			{
+				TB_Text_4->SetText(FText::FromString(NewText.Mid(8, 8)));	// 9-16 char
+			}
+			else
+			{
+				TB_Text_4->SetText(FText::GetEmpty());
+			}
+
+			if (NewText.Len() > 16)
+			{
+				TB_Text_5->SetText(FText::FromString(NewText.Mid(16, 8)));		// 17-24 char
+			}
+			else
+			{
+				TB_Text_5->SetText(FText::GetEmpty());
+			}
+
+			if (NewText.Len() > 24)
+			{
+				TB_Text_6->SetText(FText::FromString(NewText.Mid(24, 8)));		// 25-32 char
+			}
+			else
+			{
+				TB_Text_6->SetText(FText::GetEmpty());
+			}
+
+			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - No Image with up to 32 characters of text"));
+		}
+
+		//if (NewText.Len() > 17)
+		//{
+		//	// Text + Text : Index 1
+
+		//	FString FirstPart = NewText.Left(8);
+		//	FString RemainingPart = NewText.Mid(8);
+
+		//	WidgetSwitcher->SetActiveWidgetIndex(1);
+		//	TB_Text_1->SetText(FText::FromString(FirstPart));
+		//	TB_Text_2->SetText(FText::FromString(RemainingPart));
+
+		//	UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Over_8_Char"));
+		//}
+		//else
+		//{
+		//	// Image + Text : Index 0
+
+		//	FString FirstPart = NewText.Left(8);
+		//	FString RemainingPart = NewText.Mid(8);
+
+		//	WidgetSwitcher->SetActiveWidgetIndex(0);
+		//	Img_Image->SetBrushFromTexture(Texture);
+		//	TB_Text_1->SetText(FText::FromString(FirstPart));
+		//	TB_Text_2->SetText(FText::FromString(RemainingPart));
+
+		//	UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Under_8_Char"));
+		//}
 
 		// if (Texture == nullptr)
 		// {
