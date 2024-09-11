b0c10ca708a80aa9481538fd38033aa022cefb7b
fl4meboi <djkim9876@gmail.com>
2024-09-11 12:04:12 -0400

    KDJ: if/else conditions update, but still not fully working

--- Source/LanternProject/Private/Widget/LanternWidget.cpp
+++ Source/LanternProject/Private/Widget/LanternWidget.cpp
@@ -16,31 +16,12 @@ void ULanternWidget::InitWidget(UTexture2D* Texture, const FString& Text)
 	{
 		Randomize();
 		UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Randomize"));
-		// GameInstance->LogToFile(LOGTEXT(TEXT("LanternWidget::InitWidget->Randomize")));
 	}
 	else
 	{
 		FString NewText = Text.Replace(TEXT("\\n"), TEXT("\n"));
 
-		if (Texture != nullptr)			// Case 1: uploaded image, up to 16 char in 2 text boxes
-		{
-			WidgetSwitcher->SetActiveWidgetIndex(0);
-			Img_Image->SetBrushFromTexture(Texture);
-
-			TB_Text_1->SetText(FText::FromString(NewText.Left(8)));		// First 8 char
-
-			if (NewText.Len() > 8)
-			{
-				TB_Text_2->SetText(FText::FromString(NewText.Mid(8, 8)));	// Next 8 char
-			}
-			else
-			{
-				TB_Text_2->SetText(FText::GetEmpty());
-			}
-
-			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - Image with up to 16 characters of text"));
-		}
-		else		// Case 2: no image, up to 32 char in 4 text boxes
+		if (Texture == nullptr)			// Case 1: no image, up to 32 char in 4 text boxes
 		{
 			WidgetSwitcher->SetActiveWidgetIndex(1);
 
@@ -75,38 +56,31 @@ void ULanternWidget::InitWidget(UTexture2D* Texture, const FString& Text)
 
 			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - No Image with up to 32 characters of text"));
 		}
+		else if (Text.IsEmpty())		// Case 2: upload image, no text
+		{
+			WidgetSwitcher->SetActiveWidgetIndex(0);
+			Img_Image->SetBrushFromTexture(Texture);
 
-		//if (NewText.Len() > 17)
-		//{
-		//	// Text + Text : Index 1
+			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - No text"));
+		}
+		else							// Case 3: uploaded image, up to 16 char in 2 text boxes
+		{
+			WidgetSwitcher->SetActiveWidgetIndex(0);
+			Img_Image->SetBrushFromTexture(Texture);
 
-		//	FString FirstPart = NewText.Left(8);
-		//	FString RemainingPart = NewText.Mid(8);
+			TB_Text_1->SetText(FText::FromString(NewText.Left(8)));		// First 8 char
 
-		//	WidgetSwitcher->SetActiveWidgetIndex(1);
-		//	TB_Text_1->SetText(FText::FromString(FirstPart));
-		//	TB_Text_2->SetText(FText::FromString(RemainingPart));
+			if (NewText.Len() > 8)
+			{
+				TB_Text_2->SetText(FText::FromString(NewText.Mid(8, 8)));	// Next 8 char
+			}
+			else
+			{
+				TB_Text_2->SetText(FText::GetEmpty());
+			}
 
-		//	UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Over_8_Char"));
-		//}
-		//else
-		//{
-		//	// Image + Text : Index 0
-
-		//	FString FirstPart = NewText.Left(8);
-		//	FString RemainingPart = NewText.Mid(8);
-
-		//	WidgetSwitcher->SetActiveWidgetIndex(0);
-		//	Img_Image->SetBrushFromTexture(Texture);
-		//	TB_Text_1->SetText(FText::FromString(FirstPart));
-		//	TB_Text_2->SetText(FText::FromString(RemainingPart));
-
-		//	UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget-Under_8_Char"));
-		//}
-
-		// if (Texture == nullptr)
-		// {
-		// }
+			UE_LOG(LogTemp, Warning, TEXT("LanternWidget::InitWidget - Image with up to 16 characters of text"));
+		}
 	}
 }
 
@@ -119,14 +93,12 @@ void ULanternWidget::Randomize(int32 Type)
 
 	switch(Type)
 	{
-		// Text + Image
 	case 0:
-		WidgetSwitcher->SetActiveWidgetIndex(0);
+		WidgetSwitcher->SetActiveWidgetIndex(0);		// Text + Image
 		break;
 		
-		// Text + Text
 	case 1:
-		WidgetSwitcher->SetActiveWidgetIndex(1);
+		WidgetSwitcher->SetActiveWidgetIndex(1);		// Text + Text
 		break;
 
 	/*	// Image Only
